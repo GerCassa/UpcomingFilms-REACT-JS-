4e6a06d90a98060655bab43b517338cb6a078cf6
GerCassa <gercassa@gmail.com>
2018-10-29 16:03:43 +0000

    "first commit, structure created and initiated a litle relation"

---
+++ src/components/FilmList.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class FilmList extends Component {
+
+    render () {
+        return (
+            <h2>I am the proper list.</h2>
+        )
+    }
+
+}
+
+export default FilmList;
---
+++ src/containers/UpcomingFilms.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import FilmList from '../components/FilmList';
+
+class UpcomingFilms extends Component {
+
+
+    render() {
+        return(
+            <FilmList/>
+        )
+    }
+}
+
+export default UpcomingFilms;
